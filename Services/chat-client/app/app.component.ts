6172b84ca854e6f73c3dd854c1eff8e57694f912
Peter Nagy <webpmaster@gmail.com>
2016-11-10 10:18:44 +0000

    Baseline chat-client module created

---
+++ Services/chat-client/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ws-chat-app',
+    template: `
+    <alert type="info">App component is loaded</alert>
+  `,
+})
+
+export class AppComponent {
+    title = 'WS chat application';
+}
